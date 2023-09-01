3551f0820b8a2b784d331f4efb5a2a3a2c3c97bc
Jung-YouHwan <yhwjd99@gmail.com>
2023-09-01 14:59:00 +0900

    초기 설정

---
+++ src/main/java/simple_guide/SimpleGuideApplication.java
@@ -0,0 +1,13 @@
+package simple_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGuideApplication.class, args);
+    }
+
+}
